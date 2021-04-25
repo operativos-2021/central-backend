437488779c8edd0d4aa838d64a9212900b3b8b33
Jefferson000 <jefersontorres000@gmail.com>
2021-04-25 01:24:53 -0600

    game => games

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 api = Api(app)
 
-api.add_resource(games,"/game")
+api.add_resource(games,"/games")
 if __name__ == "__main__":
         app.run(debug=True) #app.run(debug=False)
