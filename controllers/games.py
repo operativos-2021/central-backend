9aeb91a8b042e5876a5a2f171a20cc2d5fcf2666
Jefferson000 <jefersontorres000@gmail.com>
2021-04-25 01:49:43 -0600

    intento de arreglo

--- controllers/games.py
+++ controllers/games.py
@@ -22,7 +22,7 @@ class games(Resource):
     def get(self):
         try:
             actual_path = os.path.dirname(os.path.abspath(__file__))
-            result_path = actual_path + "scrape_result.json"
+            result_path = actual_path + "/scrape_result.json"
             file = open(result_path)
             data = json.load(file)
             return {"data":data}
