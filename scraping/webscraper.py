a09ea4852bd59c203e12e7c6f8ff93eed88ba767
Jefferson <jefersontorres000@gmail.com>
2021-05-11 21:17:07 -0600

    enviroment variables

--- scraping/webscraper.py
+++ scraping/webscraper.py
@@ -124,9 +124,14 @@ disable_continue = None
 games_info = {}
 actual_path = os.path.dirname(os.path.abspath(__file__))
 
-op = webdriver.ChromeOptions()
-op.add_argument('headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=op)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_PATH') # GOOGLE_CHROME_PATH: /app/.apt/usr/bin/google-chrome; GOOGLE_CHROME_BIN=/app/.apt/usr/bin/google-chrome
+
+driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options) #CHROMEDRIVER_PATH:   /app/.chromedriver/bin/chromedriver CHROMEDRIVER_PATH=/app/.chromedriver/bin/chromedriver
+
 
 
 scrape_result_path = actual_path + "/scrape_result.json"
