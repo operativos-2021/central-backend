3fb58ba868fab54aaf6a9da00e6df8563841bce8
Jefferson000 <jefersontorres000@gmail.com>
2021-05-12 22:53:34 -0600

    balance advance

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import socket
 from flask_restful import Api, Resource, reqparse, abort
 import sys
 from controllers.games import games
@@ -12,7 +13,6 @@ api = Api(app)
 api.add_resource(games,"/games/<int:quantity>")
 api.add_resource(load_balance,"/load_balance/<string:games_range>")
 api.add_resource(email,"/email/") #Load balancing
-
 
 if __name__ == "__main__":
         app. run(debug=False,port=int("5000"),host='0.0.0.0') #app.run(debug=False)
--- load_balance/proccess.py
+++ load_balance/proccess.py
@@ -1,6 +1,9 @@
+import requests
+import json
 
 from time import time
 
 def getGamesFromBackend(url):
-    print("data response")
-    print(url)
+    response = requests.request("GET", url)
+    data = json.loads(response.text)
+    print(data)
